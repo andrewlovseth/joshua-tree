9a1317eb06a3635376aad1284db1fd82b4cc8ad9
Andrew Lovseth <andrew.lovseth@me.com>
2022-01-11 17:16:05 -0800

    Post: Alt Hero Treatment

--- templates/single-post/article-header.php
+++ templates/single-post/article-header.php
@@ -1,3 +1,19 @@
+<?php
+
+$alt_hero = get_field('alt_hero');
+$show = $alt_hero['show'];
+$headline_color = $alt_hero['headline_color'];
+$remove_drop_shadow = $alt_hero['remove_drop_shadow'];
+
+$className = 'post-title';
+if($show == TRUE && $remove_drop_shadow ) {
+    $className .= ' no-shadow';
+}
+
+?>
+
+
+
 <section class="article-header grid">
     <div class="featured-image">
         <div class="content">
@@ -6,6 +22,6 @@
     </div>
 
     <div class="info">
-        <h1 class="post-title"><?php the_title(); ?></h1>
+        <h1 class="<?php echo esc_attr($className); ?>"<?php if($show == TRUE): ?> style="color: <?php echo $headline_color; ?>"<?php endif; ?>><?php the_title(); ?></h1>
     </div>
 </section>
