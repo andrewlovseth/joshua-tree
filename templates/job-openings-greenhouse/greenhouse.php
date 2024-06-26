d180d8e61297782ee477acd8b38370c74443f27b
Andrew Lovseth <andrew.lovseth@me.com>
2024-06-26 15:42:03 -0700

    Remove console log

--- templates/job-openings-greenhouse/greenhouse.php
+++ templates/job-openings-greenhouse/greenhouse.php
@@ -153,7 +153,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         jobs.forEach(job => {
-            console.log(job.locations);
             let jobLocation = job.locations.map(location => {
                 return officeLabels[location] || location;
             }).join('; ');
